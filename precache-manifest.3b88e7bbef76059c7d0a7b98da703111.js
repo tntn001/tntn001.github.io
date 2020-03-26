59a9a2ab0edff0bb0a40d9ed218a7d888ffc3914
Tran Thai Ngoc <ti.en.ti0000@gmail.com>
2020-03-26 13:14:10 +0700

    Updates

---
+++ precache-manifest.3b88e7bbef76059c7d0a7b98da703111.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4883398fdb30acf171675db76360893f",
+    "url": "/my-cv/index.html"
+  },
+  {
+    "revision": "f74f170b9adb9ab14e71",
+    "url": "/my-cv/static/js/2.6141953c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-cv/static/js/2.6141953c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b583eaf56b0ad269acea",
+    "url": "/my-cv/static/js/main.45da53f7.chunk.js"
+  },
+  {
+    "revision": "baac6021d9433d8a7f42",
+    "url": "/my-cv/static/js/runtime-main.4a5acc30.js"
+  }
+]);
